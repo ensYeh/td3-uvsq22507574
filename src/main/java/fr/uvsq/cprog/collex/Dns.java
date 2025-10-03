f8261c1c02205b3572496da11dd3a683fb48c63d
BATOCFETOU Ditorgma Komla Daniel <komlabatocgmail.com>
2025-10-03 12:50:06 +0200

    code classes

--- src/main/java/fr/uvsq/cprog/collex/Dns.java
+++ src/main/java/fr/uvsq/cprog/collex/Dns.java
@@ -1,7 +1,20 @@
 package fr.uvsq.cprog.collex;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Dns {
+    private List<DnsItem> Items;
+    private String fichiersMachines;
 
+    public Dns(String fichiersMachines) throws IOException {
+        this.fichiersMachines = fichiersMachines;
+        this.Items = new ArrayList<>();
+    }
+
+    
 }
 
 

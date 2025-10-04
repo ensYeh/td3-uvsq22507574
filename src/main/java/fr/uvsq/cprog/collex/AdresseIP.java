b0dae710a6f9f74558a085749d3a8e56776fda0a
BATOCFETOU Ditorgma Komla Daniel <komlabatocgmail.com>
2025-10-04 16:28:34 +0200

    code classes

--- src/main/java/fr/uvsq/cprog/collex/AdresseIP.java
+++ src/main/java/fr/uvsq/cprog/collex/AdresseIP.java
@@ -6,12 +6,26 @@ public class AdresseIP {
     int octet3;
     int octet4;
 
-    public AdresseIP(int  octet1, int octet2, int octet3, int octet4)
+    public AdresseIP(int octet1, int octet2, int octet3, int octet4)
     {
+        if (!estoctetvalide(octet1) || !estoctetvalide(octet2) || !estoctetvalide(octet3) || !estoctetvalide(octet4)  )
+        {
+            throw new IllegalArgumentException("Adresse IP non valide. ");
+        }
+
+
         this.octet1 = octet1;
         this.octet2 = octet2;
         this.octet3 = octet3;
         this.octet4 = octet4;
+    }
+    private boolean estoctetvalide(int octet)
+    {
+        if(octet>=0 && octet<=255)
+
+            return true;
+
+        return false;
     }
 
     public int getOctet1() {
@@ -48,12 +62,7 @@ public class AdresseIP {
 
     @Override
     public String toString() {
-        this.octet1 = octet1;
-        this.octet2 = octet2;
-        this.octet3 = octet3;
-        this.octet4 = octet4;
-
-        return octet1 + "." + octet2 + "." + octet3 + "." + octet4;
+                return octet1 + "." + octet2 + "." + octet3 + "." + octet4;
     }
 }
 
--- src/main/java/fr/uvsq/cprog/collex/Dns.java
+++ src/main/java/fr/uvsq/cprog/collex/Dns.java
@@ -5,6 +5,8 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.nio.file.Files.readAllLines;
+
 public class Dns {
     private List<DnsItem> Items;
     private String fichiersMachines;
@@ -12,9 +14,23 @@ public class Dns {
     public Dns(String fichiersMachines) throws IOException {
         this.fichiersMachines = fichiersMachines;
         this.Items = new ArrayList<>();
+
+
+        // lire chaque lignes
+        List<String> lignes = readAllLines(Path.of(fichiersMachines));
+
+        for (String line : lignes) {
+            String[] parts = line.split("\t");
+            if (parts.length == 2){
+                String NomMachine = parts[0];
+                String adresseIP = parts[1];
+                String nomdomaine = "";
+                Items.add(new DnsItem(new NomMachine(NomMachine, nomdomaine), new AdresseIP()));
+            }
+        }
     }
 
-    
+
 }
 
 
